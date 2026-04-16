2dc56e4b61e9e7abe368b46975ab8aaa2cb7cfd8
jae9380 <ljy5314@gmail.com>
2026-04-16 17:42:22 +0900

    Fintracking Budget Server

---
+++ src/main/java/com/ft/budget/FintrackingBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.ft.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackingBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintrackingBudgetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ft/budget/FintrackingBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackingBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
